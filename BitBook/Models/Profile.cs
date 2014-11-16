e63d57d704d67245e5a5bfbf10ed3c9902b1e33d
moshiurimu <moshiur.imu@gmail.com>
2014-11-16 15:48:08 +0600

    BitBook Project Initially Upload[Profile & Account]

---
+++ BitBook/Models/Profile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BitBook.Models
+{
+    public class Profile
+    {
+        public int ProfileId { get; set; }
+        [Display (Name="First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Country { get; set; }
+        public string Mobile { get; set; }
+        [Display(Name = "Date Of Brith")]
+        public DateTime DateOfBirth { get; set; }
+
+        public Profile(string firstName, string lastName, string gender, string country, DateTime dateOfBirth):this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            Country = country;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public Profile()
+        {
+        }
+    }
+}
---
+++ BitBook/Models/ProfileDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BitBook.Models
+{
+    public class ProfileDbContext:DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+    }
+}
